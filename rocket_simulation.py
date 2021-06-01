0b7cec388618c758ecac90aa4e8e04df780dbae3
maximrakov <78982680+maximrakov@users.noreply.github.com>
2021-06-01 17:33:18 +0300

    Update rocket_simulation.py

--- rocket_simulation.py
+++ rocket_simulation.py
@@ -15,7 +15,7 @@ f_mass = int(input())
 f_speed = int(input()) 
 b_speed = int(input()) 
 e_mass = 6 * (10 ** 24)
-d_t = 0.09
+d_t = 1
 x = 0
 y = 0
 G = 6.67 * (10 ** (-11))
@@ -23,29 +23,40 @@ r_earth = 6371000
 r_speed = 0
 it = 0
 s_f = 600
+acceleration = 0
 tm = time.perf_counter()
-while f_mass > 0:
-    f_mass -= b_speed * d_t
+x_speed = 0
+y_speed = 0
+l = 9000
+while y > 0 or f_mass > 0:
+    if(f_mass > 0):
+      f_mass -= b_speed * d_t
     g = (G * e_mass) / ((r_earth + (x ** 2 + y ** 2) ** 0.5) ** 2)
-    acceleration = (f_speed * b_speed) / (f_mass + r_mass)
-    r_speed += (acceleration - g) * d_t
-    x_speed = r_speed * math.cos(3.14 / (180 / s_angle));
-    y_speed = r_speed * math.sin(3.14 / (180 / s_angle));
+    if(f_mass > 0):
+        acceleration = (f_speed * b_speed * d_t) / (f_mass + r_mass)
+    else:
+        acceleration = 0
+    print("accel = ", acceleration)
+    #r_speed += (acceleration) * d_t
+    
+    if not(acceleration != 0 and (acceleration * math.sin(3.14 / (180 / s_angle)) - g) * d_t < 0):
+        x_speed += (acceleration * math.cos(3.14 / (180 / s_angle))) * d_t;
+        y_speed += (acceleration * math.sin(3.14 / (180 / s_angle)) - g) * d_t;
     x += x_speed * d_t
     y += y_speed * d_t
-    if time.perf_counter() - tm >= 1:
-        print("coord= ",x, y)
-        print("speed_x= ", x_speed , "speed_y= ", y_speed);
-        print("mod speed = ", r_speed)
-        print("fuel mass = ", f_mass)
-        print()
-        print()
-        tm = time.perf_counter()
-    canvas.create_oval(s_f - x / 40,s_f - y / 40,s_f - x / 40,s_f  - y / 40, fill='green')
+    # if time.perf_counter() - tm >= 1:
+    print("coord= ",x, y)
+    print("speed_x= ", x_speed , "speed_y= ", y_speed);
+    print("mod speed = ", r_speed)
+    print("fuel mass = ", f_mass)
+    print()
+    print()
+    tm = time.perf_counter()
+    canvas.create_oval(s_f - x / l,s_f - y / l,s_f - x /l,s_f  - y / l, fill='green')
     root.update()
 
-#30
-#33000
-#270000
+#45
 #4000
-#800
+#150000
+#2500
+#650
